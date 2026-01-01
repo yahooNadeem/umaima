169348aaa5b4e26c2cc907a2ef2e1fb468a2c280
Administrator <Administrator@EC2AMAZ-8UDHBDS>
2026-01-01 20:32:21 +0000

    push to push1

--- securitygrouprule.tf
+++ securitygrouprule.tf
@@ -5,5 +5,5 @@ resource "aws_security_group_rule" "rdp_access" {
   protocol          = "tcp"
   cidr_blocks       = ["10.0.0.6/32"]
   security_group_id = data.aws_security_group.terrasg.id
-  description       = "Allow RDP access from terradevmachine"
+  description       = "Allow only RDP access from terradevmachine"
 }
