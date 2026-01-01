48a106725bf00634fa8df974794812c2d26f3f44
Administrator <Administrator@EC2AMAZ-8UDHBDS>
2026-01-01 19:52:48 +0000

    push to re-check

--- securitygrouprule.tf
+++ securitygrouprule.tf
@@ -7,17 +7,6 @@ resource "aws_security_group_rule" "rdp_access" {
   security_group_id = data.aws_security_group.terrasg.id
   description       = "Allow RDP access from terradevmachine"
 }
-
-resource "aws_security_group_rule" "SMBaccess" {
-  type              = "ingress"
-  from_port         = 445
-  to_port           = 445
-  protocol          = "tcp"
-  cidr_blocks       = ["10.0.0.9/32"]
-  security_group_id = data.aws_security_group.terrasg.id
-  description       = "Allow SMB access from terradevmachine"
-}
-
 resource "aws_security_group_rule" "SMB_access" {
   type              = "ingress"
   from_port         = 445
