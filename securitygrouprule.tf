2972b9b0361cb985cc1e29c22d5eab1c7930f70b
Administrator <Administrator@EC2AMAZ-8UDHBDS>
2026-01-04 15:08:39 +0000

    Merge5

--- securitygrouprule.tf
+++ securitygrouprule.tf
@@ -7,3 +7,15 @@ resource "aws_security_group_rule" "rdp_access" {
   security_group_id = data.aws_security_group.terrasg.id
   description       = "Allow only RDP access from terradevmachine"
 }
+
+
+# ✅ New SMB rule
+resource "aws_security_group_rule" "smb_access" {
+  type              = "ingress"
+  from_port         = 445
+  to_port           = 445
+  protocol          = "tcp"
+  cidr_blocks       = ["10.0.0.9/32"]
+  security_group_id = data.aws_security_group.terrasg.id
+  description       = "Allow SMB access from terramachine"
+}
